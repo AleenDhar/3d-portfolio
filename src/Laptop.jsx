4b370e852f0cb553366f3fce018f223b74e58677
Aleen Dhar <86429480+AleenDhar@users.noreply.github.com>
2024-03-02 07:52:48 +0530

    3rd commit

--- src/Laptop.jsx
+++ src/Laptop.jsx
@@ -59,29 +59,22 @@ export default function Laptop(){
         <pointLight position={[0,0,-.63]} intensity={40} color={'#B22222'}/>
         <Environment  map={map} preset="warehouse"/>
         <pointLight position={[0,10,3.7]} intensity={40} color={'#6495ED'}/>
-    <group >
-    <Text
-        font="fonts/Caprasimo-Regular.ttf"
-        fontSize={0.3}
-        position-z={0.1}
-        anchorY={"bottom"}
-    >
-        {""}
-        <meshBasicMaterial color={'#FFFAF0'} toneMapped={false} />
-    </Text>
-        <RoundedBox args={[4.8,2.7,.1]} position={[0,-.1,0.01]}>
-        <MeshPortalMaterial position={[0,0,3]}>
-        <ambientLight intensity={10}/>
-            <mesh>
-            <sphereGeometry  args={[1,64,64]} />
-            {/* <OrbitControls autoRotate={true}/> */}
-            <meshStandardMaterial map={map}   side={THREE.BackSide}/>
-            </mesh>
-        </MeshPortalMaterial>
-        </RoundedBox>
-    </group>
+
     
-        <primitive object={laptop.scene} ref={target} position={[0,-3.7,3.7]}/>
+        <primitive object={laptop.scene} ref={target} position={[0,-3.7,3.7]}>
+            <Html
+            wrapperClass="laptop"
+            position={[.01,3.65,-3.69]}
+            transform   
+            occlude
+            distanceFactor={1.65}   
+            // rotation-x={-.25}
+            >
+                <div className="wrapper" onPointerDown={(e) => e.stopPropagation()}>
+                <iframe src="https://portfolio-ochre-five-20.vercel.app/"/>
+                </div>
+            </Html>
+        </primitive>
         </>
     )
 }
